4c56db21bc9b94a150b5ac1fd9e9a002e0dbabaf
sionziom <adriansionek95@gmail\.com>
2020-09-08 21:51:12 +0200

    heroku refactor

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://salty-spire-97311.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://salty-spire-97311.herokuapp.com/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
